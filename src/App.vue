389d4ee78a2a99cc637f6a65e187af94ea656c01
Gaston Feldick <gastonfeldick@gmail.com>
2024-04-27 21:50:46 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/Navbar.vue'
+
+</script>
+
+<template>
+    <Navbar/>
+
+  <div class="container overflow-x-hidden">
+  </div>
+</template>
+
+
+<!-- 
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
